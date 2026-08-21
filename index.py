d1d3efcbb3b933b67173fc929e8eaf1bd790f5d2
Kydorpe_escritorio <lopedro45@gmail.com>
2026-08-21 08:14:35 -0300

    update Tkinter para Ctk

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-from tkinter import *
+import customtkinter as ctk
 
-windows = Tk() 
+app = ctk.CTk() 
 
 #Palet colors
 color1 = '#F3D38A'
@@ -8,20 +8,19 @@ color2 = '#CAE7B9'
 color3 = '#EB9486'
 color4 = '#7E7F9A'
 
-windows.title('LiBok')
-windows.geometry('1000x900')
+app.title('LiBok')
+app.geometry('1000x900')
 
-windows.config(background=color1)
-windows.iconphoto(False, PhotoImage(file='img/icon.png'))
+app.config(background=color1)
 
-label_dashboard = Label  (windows,width=10, height=2,text='Dashboard',font=('Inter'),bg=color2)
+label_dashboard = ctk.CTkLabel(app,width=10, height=2,text='Dashboard',font=('Inter',20),fg_color=color2)
 label_dashboard.grid(row=0,column=0)
 
-label_search = Label  (windows,width=10, height=2,text='Pesquisar',font=('Inter'),bg=color2)
+label_search = ctk.CTkLabel(app,width=10, height=2,text='Pesquisar',font=('Inter',20),fg_color=color2)
 label_search.grid(row=0,column=1)
 
-label_register = Label  (windows,width=10, height=2,text='Cadastrar',font=('Inter'),bg=color2)
+label_register = ctk.CTkLabel(app,width=10, height=2,text='Cadastrar',font=('Inter',20),fg_color=color2)
 label_register.grid(row=0,column=2)
 
 
-windows.mainloop()
+app.mainloop()
